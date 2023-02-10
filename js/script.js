4e40d942f7578190a49b245e2a9b8bd54819a430
MaroMohamedSalah <91625271+MaroMohamedSalah@users.noreply.github.com>
2023-02-10 15:01:40 +0200

    delete unused files

--- js/script.js
+++ js/script.js
@@ -261,22 +261,11 @@ function handelColorChange() {
 }
 
 // Install sw
-const registerServiceWorker = async () => {
-	if ("serviceWorker" in navigator) {
-		try {
-			const registration = await navigator.serviceWorker.register("/sw.js", {
-				scope: "/",
-			});
-			if (registration.installing) {
-				console.log("Service worker installing");
-			} else if (registration.waiting) {
-				console.log("Service worker installed");
-			} else if (registration.active) {
-				console.log("Service worker active");
-			}
-		} catch (error) {
-			console.error(`Registration failed with ${error}`);
-		}
-	}
-};
-registerServiceWorker();
+if (navigator.serviceWorker) {
+	addEventListener("load", () => {
+		navigator.serviceWorker
+			.register("../sw.js")
+			.then((reg) => console.log("service worker registed", reg))
+			.catch((err) => console.log(err));
+	});
+}
--- sw.js
+++ sw.js
@@ -1,60 +1,57 @@
 const cacheName = "tasbeeh-v0";
-asserts = [
+assist = [
 	"/",
+	"/index.html",
+	"/audio",
 	"/css/all.min.css",
 	"/css/normalize.css",
 	"/css/style.css",
-	"/css/style2.css",
-	"/pages/tasbeeh.html",
-	"/index.html",
-	"/script.js",
-	"/sw.js",
-	"/audio",
-	"/img/mosque-solid.svg",
 	"/img/nwdn_file_temp_1605338466849 copy.jpg",
-	"/img/WhatsApp Image 2022-03-21 at 7.18.56 PM copy.jpg",
-	"/img/⌛.svg",
-	"/img/✔️.svg",
-	"/img/❤.svg",
-	"/img/👳.svg",
-	"/img/💧.svg",
 	"/img/📿.svg",
+	"/js/script.js",
+	"/webfonts",
+	"/manifest.json",
+	"/sw.js",
 ];
 
-// Install sw
-const addResourcesToCache = async (resources) => {
-	const cache = await caches.open(cacheName);
-	await cache.addAll(resources);
-};
-
+// add assist in the caches
 self.addEventListener("install", (event) => {
-	event.waitUntil(addResourcesToCache(assets));
-});
-
-// Activate sw
-self.addEventListener("activate", (activateEvent) => {
-	// Delete all old caches
-	caches.keys().then((keys) => {
-		return Promise.all(
-			keys.filter((key) => key != cacheName).map((key) => caches.delete(key))
-		);
-	});
-});
-
-// fetch sw
-self.addEventListener("fetch", (fetchEvent) => {
-	fetchEvent.respondWith(
-		caches.match(fetchEvent.request).then((res) => {
-			return (
-				res ||
-				fetch(fetchEvent.request).then((fetchRes) => {
-					if (!(fetchEvent.request.url.indexOf("http") === 0)) return;
-					return caches.open(cacheName).then((cache) => {
-						cache.put(fetchEvent.request, fetchRes.clone());
-						return fetchRes;
-					});
-				})
-			);
-		})
+	event.waitUntil(
+		caches
+			.open(cacheName)
+			.then((cache) => cache.addAll(assist))
+			.catch((err) => console.log(err))
 	);
 });
+
+// self.addEventListener("install", (event) => {
+// 	event.waitUntil(addResourcesToCache(assets));
+// });
+
+// // Activate sw
+// self.addEventListener("activate", (activateEvent) => {
+// 	// Delete all old caches
+// 	caches.keys().then((keys) => {
+// 		return Promise.all(
+// 			keys.filter((key) => key != cacheName).map((key) => caches.delete(key))
+// 		);
+// 	});
+// });
+
+// // fetch sw
+// self.addEventListener("fetch", (fetchEvent) => {
+// 	fetchEvent.respondWith(
+// 		caches.match(fetchEvent.request).then((res) => {
+// 			return (
+// 				res ||
+// 				fetch(fetchEvent.request).then((fetchRes) => {
+// 					if (!(fetchEvent.request.url.indexOf("http") === 0)) return;
+// 					return caches.open(cacheName).then((cache) => {
+// 						cache.put(fetchEvent.request, fetchRes.clone());
+// 						return fetchRes;
+// 					});
+// 				})
+// 			);
+// 		})
+// 	);
+// });
